157a6b4c67d98a0f73bbf6d3ad4df19471e5270b
Lilit <melikyanlilit740@gmail.com>
2021-02-09 19:00:11 +0400

    First comit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import  React, { useState } from 'react';
+import 'normalize.css';
+import "./style/header.module.css";
+import Header1 from "./components/Header1";
+function App() { 
+  return (
+    <div className="App">
+      <div className="wrapper">
+        
+        <Header1/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
